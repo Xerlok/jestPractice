db09dbfe43d6994f045e6048ecd33f20d0f942d9
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-05-06 23:09:07 +0300

    test/feat: make caesarCipher and main cases
    Cipher can cipher single words with first upperCase
    letter, but only if shift doesn't overflow.

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,4 +1,27 @@
 /*eslint-disable*/
 export default function caesarCipher(string, shift) {
-  
+  const alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u',
+    'v', 'w', 'x', 'y', 'z'
+  ];
+
+  const word = [];
+
+  if (!string || typeof string !== 'string') { return null; }
+  if (!shift) { shift = 3 }
+
+  for (let i = 0; i < string.length; i += 1) {
+    word.push(string.charAt(i));
+  }
+
+  for (let i = 0; i < word.length; i += 1) {
+    word[i] = word[i].toLowerCase();
+    word[i] = alphabet.indexOf(word[i]);
+    word[i] = alphabet[word[i] + shift];
+  }
+
+  word[0] = word[0].toUpperCase();
+  return word.join('');
 }
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,11 +1,29 @@
 /*eslint-disable*/
+import { experiments } from 'webpack';
 import caesarCipher from './caesarCipher';
 
 test('function is defined', () => {
   expect(caesarCipher()).toBeDefined();
 })
 
+test('handles null string', () => {
+  expect(caesarCipher(2)).toBe(null);
+})
+
+test('handles null shift', () => {{
+  expect(caesarCipher('Kek')).toBe('Nhn');
+}})
+
 test('returns ciphered string', () => {
   expect(caesarCipher('Kek', 7)).toBe('Rlr');
   expect(caesarCipher('Sonic', 4)).toBe('Wsrmg');
+})
+
+test('handles shift overflow', () => {
+  expect(caesarCipher('Kek', 155)).toBe('Jdj');
+})
+
+test('returns correct upper cases', () => {
+  expect(caesarCipher('mAndAloriAn', 9)).toBe('vJwmJuxarJw');
+  expect(caesarCipher('PROcrastiNATION', 8).toBe('XZWkziabqvIBQWV'));
 })
