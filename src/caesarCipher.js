dfb4a3908d32b1f0cda479e20c4cbf7b3b76f4ce
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-05-09 00:06:31 +0300

    feat/test: make it return correct lower/upper case

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-export default function caesarCipher(string, shift) {
+function caesarCipher(string, shift) {
   const alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n',
@@ -17,11 +17,39 @@ export default function caesarCipher(string, shift) {
   }
 
   for (let i = 0; i < word.length; i += 1) {
+    const LowerCase = isLowerCase(word[i]);
+    const UpperCase = isUpperCase(word[i]);
+
     word[i] = word[i].toLowerCase();
     word[i] = alphabet.indexOf(word[i]);
-    word[i] = alphabet[word[i] + shift];
+    const moduloOfIndex = (word[i] + shift) % 26;
+    if (LowerCase) {
+      word[i] = alphabet[moduloOfIndex];
+    }
+    if (UpperCase) {
+      word[i] = alphabet[moduloOfIndex].toUpperCase();
+    }
   }
 
-  word[0] = word[0].toUpperCase();
   return word.join('');
+}
+
+function isUpperCase(letter) {
+  if (letter === letter.toUpperCase()
+    && letter !== letter.toLowerCase()) {
+      return true;
+  }
+  return false;
+}
+
+function isLowerCase(letter) {
+  if (letter === letter.toLowerCase()
+    && letter !== letter.toUpperCase()) {
+      return true;
+  }
+  return false;
+}
+
+export {
+  caesarCipher,
 }
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,29 +1,33 @@
 /*eslint-disable*/
 import { experiments } from 'webpack';
-import caesarCipher from './caesarCipher';
+import { caesarCipher } from './caesarCipher';
 
 test('function is defined', () => {
   expect(caesarCipher()).toBeDefined();
-})
+});
 
 test('handles null string', () => {
   expect(caesarCipher(2)).toBe(null);
-})
+});
 
 test('handles null shift', () => {{
   expect(caesarCipher('Kek')).toBe('Nhn');
-}})
+}});
 
 test('returns ciphered string', () => {
   expect(caesarCipher('Kek', 7)).toBe('Rlr');
   expect(caesarCipher('Sonic', 4)).toBe('Wsrmg');
-})
+});
 
-test('handles shift overflow', () => {
+test('handles positive shift overflow', () => {
   expect(caesarCipher('Kek', 155)).toBe('Jdj');
-})
+});
 
 test('returns correct upper cases', () => {
   expect(caesarCipher('mAndAloriAn', 9)).toBe('vJwmJuxarJw');
-  expect(caesarCipher('PROcrastiNATION', 8).toBe('XZWkziabqvIBQWV'));
-})
+  expect(caesarCipher('PROcrastiNATION', 8)).toBe('XZWkziabqVIBQWV');
+});
+
+test('returns correct punctuation', () => {
+  expect(caesarCipher('!gIMba,l.')).toBe('!jLPed,o.');
+});
